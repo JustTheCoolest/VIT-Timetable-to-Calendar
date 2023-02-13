e4eac30ae950a0e3df8e90c28531fdc6eb3d0bdc
JustTheCoolest <balu.somenumbers@gmail.com>
2023-02-14 01:05:11 +0530

    Design change: Instead of creating a slot info file manually, this code will directly read the timetable table text too to always create the right timetable irrespective of changes made in the semester.
    Also, changes to courses object to store more info and be indexable by course code.

--- Backend/calendar_generator.py
+++ Backend/calendar_generator.py
@@ -1,34 +1,30 @@
 import icalendar
 
 
-def get_courses(text: str) -> list[dict]:
+def get_courses(text: str) -> dict[str, dict]:
     """
     Converts the text copied from the course list in, VTopCC >> Academics >> Time Table, to a list of courses with the
-    relevant data alone.
-    Omits courses that do not have a slot assigned to them (like non-graded courses)
+    relevant data.
     """
     data = text.splitlines()
     start_index = data.index('1')
-    courses = []
+    courses = {}
     for line_index in range(start_index, len(data), 31):
         slot = data[line_index + 15][:-2]
-        if slot == 'NIL':
-            continue
         header = (data[line_index + 4].split(' - '))
-        course = {
-            'code': header[0],
+        slot = header[0]
+        courses[slot] = {
             'title': header[1],
+            'LTPJC': tuple(map(float, data[line_index + 8].split())),
+            'class_code': data[line_index + 13],
             'slot': slot,
             'venue': data[line_index + 18],
             'professor': data[line_index + 20][:-2]
         }
-        courses.append(course)
     return courses
 
 
-def generate_ical(courses: list[dict]) -> icalendar.cal.Calendar:
-    return
-
-
-def generate_ics(ical: icalendar.cal.Calendar) -> str:
-    return
+def generate_calendar(courses: list[dict]) -> icalendar.cal.Calendar:
+    calendar = icalendar.Calendar()
+    calendar['prodid'] = '-// Andhavarapu Balu // github.com/JustTheCoolest/VIT-Chennai-Timetable-to-Calendar //EN'
+    calendar['x-wr-timezone'] = 'Asia/Kolkata'
