38b3b5a1c33d44cfd9d07ddd0d1846e18ec45528
JustTheCoolest <balu.somenumbers@gmail.com>
2023-03-27 01:09:44 +0530

    Objective for Discord rolemenu creator

---
+++ Frontend/discord_rolemenu_creation.py
@@ -0,0 +1,16 @@
+"""
+This module interfaces with Discord to create:
+1) A set of cosmetic roles in the bot's server based on the course list given,
+2) Empty private channels for the corresponding subject roles, and
+3) A message that we can copy-paste as the rolemenu message.
+This bot is only intended to work as a complement of other rolemenu creation bots like Carl bot, by automating the part
+of the process where we create the channel, while allowing the other bot to handle the menu operations by itself.
+"""
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
