a66dbb8fcdb77447c1f6db29d0615173d940ff92
JustTheCoolest <balu.somenumbers@gmail.com>
2023-03-27 18:04:23 +0530

    Creating role and text channel on Discord (untested). Pending: assign right category and handle exceptions for channel already existing

--- Frontend/discord_rolemenu_creation.py
+++ Frontend/discord_rolemenu_creation.py
@@ -6,10 +6,12 @@ This module interfaces with Discord to create:
 This bot is only intended to work as a complement of other rolemenu creation bots like Carl bot, by automating the part
 of the process where we create the channel, while allowing the other bot to handle the menu operations by itself.
 """
+import asyncio
 
 import discord
 from Backend import calendar_generator
 import discord_tokens
+
 # discord_tokens is a file that contains the following variables: bot_token, active_server_id, courses_text
 
 
@@ -29,11 +31,25 @@ def filter_role_names(role_names, guild):
     return [role_name for role_name in role_names if role_name not in existing_roles]
 
 
+async def create_role_and_channels(role_name, guild, all_channel_role=None):
+    role = guild.create_role(name=role_name)
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        role: discord.PermissionOverwrite(read_messages=True)
+    }
+    if all_channel_role:
+        overwrites[all_channel_role] = discord.PermissionOverwrite(read_messages=True)
+    await guild.create_text_channel(role_name, overwrites=overwrites)
+
+
 @client.event
 async def on_ready():
     role_names = get_role_names()
     guild = client.get_guild(discord_tokens.active_server_id)
     role_names = filter_role_names(role_names, guild)
+    all_channel_role = discord.utils.get(guild.roles, name='All Channel')
+    await asyncio.gather(*[create_role_and_channels(role_name, guild, all_channel_role)
+                           for role_name in role_names])
     await client.close()
 
 
