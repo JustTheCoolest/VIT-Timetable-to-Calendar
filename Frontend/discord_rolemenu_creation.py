3062b59d265deb508bca666a888812db3d047a4b
JustTheCoolest <balu.somenumbers@gmail.com>
2023-03-27 14:39:38 +0530

    Connecting to Discord bot + Creating role list based on course list and existing roles

--- Frontend/discord_rolemenu_creation.py
+++ Frontend/discord_rolemenu_creation.py
@@ -7,9 +7,38 @@ This bot is only intended to work as a complement of other rolemenu creation bot
 of the process where we create the channel, while allowing the other bot to handle the menu operations by itself.
 """
 
+import discord
+from Backend import calendar_generator
+import discord_tokens
+# discord_tokens is a file that contains the following variables: bot_token, active_server_id, courses_text
+
+
+client = discord.Client(intents=discord.Intents.default())
+
+
+def get_role_names():
+    courses_text = discord_tokens.courses_text
+    courses = calendar_generator.get_courses(courses_text)
+    course_codes = list(courses.keys())
+    course_codes.sort()
+    return course_codes
+
+
+def filter_role_names(role_names, guild):
+    existing_roles = [role.name for role in guild.roles]
+    return [role_name for role_name in role_names if role_name not in existing_roles]
+
+
+@client.event
+async def on_ready():
+    role_names = get_role_names()
+    guild = client.get_guild(discord_tokens.active_server_id)
+    role_names = filter_role_names(role_names, guild)
+    await client.close()
+
 
 def main():
-    return
+    client.run(discord_tokens.bot_token)
 
 
 if __name__ == "__main__":
