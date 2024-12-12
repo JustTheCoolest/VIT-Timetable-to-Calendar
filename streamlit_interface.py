8e1db4f8cf78a0e1e5ebd49d63a6c1abdb128c14
Nafis266 <nafis.niyas@gmail.com>
2024-12-12 22:37:43 +0530

    added images and youtube links

--- streamlit_interface.py
+++ streamlit_interface.py
@@ -7,7 +7,15 @@ def main():
     st.title("VIT Time Table to iCal Converter")
     st.text("Made by Andhavarapu Balu")
     st.markdown("[GitHub repository](https://github.com/JustTheCoolest/VIT-Timetable-to-Calendar)")
-    
+
+    st.markdown("[Google Calendar desktop tutorial](https://youtu.be/A3Rubu_3Le0?si=FA482m6ABF9n7szG)")
+    st.markdown("[iPadOS with Apple Calendar tutorial](https://youtu.be/dafPgd-1Z98)")
+
+    st.text("Example:")
+    st.image("images/pg1_stream.png")
+    st.image("images/pg2_stream.png")
+
+    st.text("Select all the text shown in the above images from SI.No to L94")
     page_text = st.text_area("Paste the text copied from the page here:")
 
     if not page_text:
@@ -24,9 +32,9 @@ def main():
         mime="text/calendar"
     )
     
-    st.image("instructions/images/pg1 image.png")
-    st.image("instructions/images/pg2 image.png")
-    st.text("select all the text shown in the above images from SI.No to L94")
+
+    
+
     
 
 main()
