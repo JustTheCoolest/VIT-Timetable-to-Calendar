e6d644e48ff83ec86b68e660d451644e47d406a8
JustTheCoolest <balu.somenumbers@gmail.com>
2025-05-27 18:21:42 +0530

    organised into expanders

--- streamlit_interface.py
+++ streamlit_interface.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 
 import streamlit as st
 from google.cloud import firestore
@@ -17,31 +18,7 @@ def get_firestore_downloads_doc_ref():
 
     return doc_ref
 
-
-def streamlit_stuff(downloads_doc_ref):
-    st.title("VIT Time Table to iCal Converter")
-    st.text("Made by Andhavarapu Balu")
-    st.markdown("[GitHub repository](https://github.com/JustTheCoolest/VIT-Timetable-to-Calendar)")
-
-    st.subheader("Instructions")
-    st.text("1. Copy all the text from your VTOP timetable page from top to bottom (SI.No to L94)")
-    st.text("2. Paste the copied text in the section below and click outsite the box (or click Ctrl+Enter)")
-    st.text("3. Download the file (.ics) and import it into your preferred calendar service")
-
-    st.markdown("""
-**Refer the video tutorials below for detailed instructions**\n
-[Google Calendar desktop tutorial](https://youtu.be/A3Rubu_3Le0?si=FA482m6ABF9n7szG)\n
-[iPadOS with Apple Calendar tutorial](https://youtu.be/dafPgd-1Z98)\n
-""")
-
-    page_text = st.text_area("Paste the text copied from the page here:")
-
-    count_to_display = round(downloads_doc_ref.get().to_dict().get('download_count'), 1)
-    st.subheader(f"{count_to_display}+ downloads so far!")
-
-    if not page_text:
-        return
-
+def provide_download(page_text, downloads_doc_ref):
     start_date = (datetime.datetime.now() - datetime.timedelta(days=1)).date()
     end_date = datetime.date(2025, 5, 31)
     ics_text = calendar_generator.generate_calendar(page_text, [start_date, end_date])
@@ -56,6 +33,43 @@ def streamlit_stuff(downloads_doc_ref):
             "download_count": firestore.Increment(1)
         })
     )
+
+def provide_samples_expander():
+    valid_extensions = ('.png', '.jpg', '.jpeg', '.gif')
+    with st.expander("Use Cases (Screenshots)"):
+        for sample in sorted(os.listdir("sample_screenshots")):
+            if not sample.endswith(valid_extensions):
+                continue
+            st.image(f"sample_screenshots/{sample}")
+
+def provide_instructions_expander():
+    with st.expander("Instructions"):
+        st.text("1. Copy all the text from your VTOP timetable page from top to bottom (\"SI.No\" to \"L94\")")
+        st.text("2. Paste the copied text in the section below and click outside the box (or click Ctrl+Enter)")
+        st.text("3. Download the file (.ics) and import it into your preferred calendar service")
+
+        st.markdown("""
+**Refer the video tutorials below for detailed instructions**\n
+[Google Calendar desktop tutorial](https://youtu.be/A3Rubu_3Le0?si=FA482m6ABF9n7szG)\n
+[iPadOS with Apple Calendar tutorial](https://youtu.be/dafPgd-1Z98)\n
+""")
+
+def streamlit_stuff(downloads_doc_ref):
+    st.title("VIT Time Table to iCal Converter")
+    st.text("Made by Andhavarapu Balu")
+    st.markdown("[GitHub repository](https://github.com/JustTheCoolest/VIT-Timetable-to-Calendar)")
+
+    provide_instructions_expander()
+
+    page_text = st.text_area("Paste the text copied from the timetable page here:")
+
+    count_to_display = round(downloads_doc_ref.get().to_dict().get('download_count'), 1)
+    st.subheader(f"{count_to_display}+ downloads so far!")
+
+    if page_text:
+        provide_download(page_text, downloads_doc_ref)
+
+    provide_samples_expander()
 
 def main():
     doc_ref = get_firestore_downloads_doc_ref()
